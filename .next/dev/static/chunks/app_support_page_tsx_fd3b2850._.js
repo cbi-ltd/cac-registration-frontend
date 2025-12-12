49d1b93995ee5f4b199469f752f89d959e249fe6
AkinCbi <akinlolu.odeku@cbitechnologiesltd.com>
2025-12-12 13:48:05 +0100

    Refactor code structure for improved readability and maintainability

--- .next/dev/static/chunks/app_support_page_tsx_fd3b2850._.js
+++ .next/dev/static/chunks/app_support_page_tsx_fd3b2850._.js
@@ -1,7 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/_96e2b9d2._.js"
+  "static/chunks/node_modules_562ab3b0._.js",
+  "static/chunks/_98a7eb1b._.js"
 ],
     source: "dynamic"
 });
